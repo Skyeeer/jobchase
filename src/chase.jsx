46810b0b12a92c008c97bb73d9cd9f5aafc199f5
Chalel <charlie03.pl@gmail.com>
2024-03-11 13:24:59 +0100

    funktionell fungerar

--- src/chase.jsx
+++ src/chase.jsx
@@ -1,8 +1,10 @@
 // App.js
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.png';
 import './style.css';
 import bookmark from './bookmark.png';
+import jobs from './jobs.json';
+console.log(jobs);
 
 const Header = () => {
     return (
@@ -13,48 +15,62 @@ const Header = () => {
     );
 };
 
-const Aside = () => {
+const Aside = ({ onCardClick }) => {
     return (
         <aside>
-            <h2>This is the aside</h2>
             <div className='cardContainer'>
                 <ul>
-                    <li>
-                        <div className='card'><div className='cardhead'>
-                            <h2 className='position'>Position</h2>
-                            <img className='bookmark' src={bookmark} width={30} height={30} alt='Bookmark'></img>
-                        </div>
-                            <p className='org'>Company</p>
-                            <p className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error illo ea vero maxime maiores temporibus.</p>
-                            <div className='tags'>
-                                <div className='tag'>Sweden</div>
-                                <div className='tag'>Part time</div>
-                                <div className='tag'>Junior</div>
-                                <div className='tag'>Javascript</div>
+                    {jobs.map((job) => (
+                        <li key={job.id} onClick={() => onCardClick(job)}>
+                            <div className='card'>
+                                <div className='cardhead'>
+                                    <h2 className='position'>{job.position}</h2>
+                                    <img className='bookmark' src={bookmark} width={30} height={30} alt='Bookmark'></img>
+                                </div>
+                                <p className='org'>{job.company}</p>
+                                <p className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error illo ea vero maxime maiores temporibus.</p>
+                                <div className='tags'>
+                                    <div className='tag'>{job.location}</div>
+                                    <div className='tag'>{job.contract}</div>
+                                    <div className='tag'>{job.level}</div>
+                                    {job.languages.map((language) => (
+                                        <div className='tag' key={language}>{language}</div>
+                                    ))}
+                                </div>
                             </div>
-                        </div>
-                    </li>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </aside >
     );
 };
 
-const Main = () => {
+const Main = ({ job }) => {
+    if (!job) return <main>Select a job to see details :)</main>;
+
+
     return (
         <main>
-            <h2>This is the main content</h2>
+            <h2>{job.position} at {job.company}</h2>
+            <p>{job.description || 'Job description goes here.'}</p>
         </main>
     );
 };
 
 const App = () => {
+
+    const [selectedJob, setSelectedJob] = useState(null);
+    const handleCardCheck = (job) => {
+        setSelectedJob(job);
+    };
+
     return (
         <div>
             <Header />
             <div style={{ display: 'flex' }}>
-                <Aside />
-                <Main />
+                <Aside onCardClick={handleCardCheck} />
+                <Main job={selectedJob} />
             </div>
         </div>
     );
