98d12633dd4a0ba8fac7f7a1a8c03da20f0dcc5f
Chalel <charlie03.pl@gmail.com>
2024-03-15 14:27:32 +0100

    fetch

--- src/chase.jsx
+++ src/chase.jsx
@@ -3,8 +3,8 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.png';
 import './style.css';
 import bookmark from './bookmark.png';
-import jobs from './jobs.json';
-console.log(jobs);
+// import jobs from './jobs.json';
+// console.log(jobs);
 
 const Header = ({ onSearchChange }) => {
     return (
@@ -50,7 +50,7 @@ const Aside = ({ jobs, onCardClick }) => {
 const Main = ({ job }) => {
     if (!job) return <main></main>;
 
-
+    //************MÅSTE STYLAS************
     return (
         <main>
             <div className='article'>
@@ -70,6 +70,7 @@ const App = () => {
     // const [jobs, setJobs] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
     const [selectedJob, setSelectedJob] = useState(null);
+    const [jobs, setJobs] = useState([]);
     const handleCardCheck = (job) => {
         setSelectedJob(job);
     };
@@ -77,22 +78,20 @@ const App = () => {
         setSearchTerm(term.toLowerCase());
     };
 
+    //FETCH METOD
+    useEffect(() => {
+        fetch('jobs.json')
+            .then(response => response.json())
+            .then(data => setJobs(data))
+            .catch(error => console.error('Error with fetching', error));
+    }, []);
+
     const filteredJobs = jobs.filter((job) =>
         job.location.toLowerCase().includes(searchTerm) ||
         job.contract.toLowerCase().includes(searchTerm) ||
         job.level.toLowerCase().includes(searchTerm) ||
         job.languages.some(language => language.toLowerCase().includes(searchTerm))
     );
-
-    // useEffect(() => {
-    //     fetch('jobs.json')
-    //         .then(response => response.json())
-    //         .then(data => {
-    //             setJobs(data);
-    //         })
-    //         .catch(error => console.error("JSON fetch failed to load", error));
-    // }, []);
-
 
 
     return (
