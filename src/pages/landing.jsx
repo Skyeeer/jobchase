43f1f95c8592d2cee9b2b6a6539b3d0e54ed8290
Skyeeer <charlie03.pl@gmail.com>
2024-04-08 17:14:48 +0200

    added alt to img

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -12,7 +12,7 @@ const LandingPage = () => {
             <div className='flex-grow mt-[-6rem]'>
                 <div className='flex flex-col items-center justify-center h-full'>
                     <h1 className='text-center text-xl font-bold'>Welcome to</h1>
-                    <img src={logo} className='w-64' />
+                    <img src={logo} alt='logo' className='w-64' />
                     <Link to="/SignUpPage" className="bg-lime-500 text-white px-6 py-3 rounded-full cursor-pointer hover:bg-lime-600 mt-4">Sign Up</Link>
                 </div>
             </div>
